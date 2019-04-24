801bd02b42794f429761aa5ed5bddfab97836da8
zarinaomor <zarinaomorova@yahoo.com>
2019-04-24 09:05:01 -0700

    create new page

--- controllers/userC.js
+++ controllers/userC.js
@@ -3,5 +3,20 @@ const router = express.Router();
 const User = require('../models/user');
 
 
+router.get('/new', (req,res) => {
+    User.find({}, (err, allUsers) => {
+        if(err) {
+            res.send(err);
+        } else {
+            res.render('new.ejs', {
+                user: allUsers
+            });
+        }
+    });
+})
+
+
+
+
 
 module.exports = router;
