acd599be080d0c0c5c14050988d5136c0f7bea90
zarinaomor <zarinaomorova@yahoo.com>
2019-04-25 10:54:50 -0700

    changes

--- controllers/authC.js
+++ controllers/authC.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 
-router.get('/', (req,res)=>{
+router.get('/login', (req,res)=>{
     res.render('login.ejs')
 })
 
