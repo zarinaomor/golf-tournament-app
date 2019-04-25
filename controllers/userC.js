3cf259ce4044f95844a0ef8b55c52c8f287ed528
zarinaomor <zarinaomorova@yahoo.com>
2019-04-25 16:48:32 -0700

    css for show page

--- controllers/userC.js
+++ controllers/userC.js
@@ -14,63 +14,8 @@ router.get('/new', (req,res) => {
             }
         });
     })
-1
-router.post('/', async (req, res) => {
-    try {
-        const foundUser = await User.findOne({'email': req.body.email});
-        console.log(foundUser)
-        if(foundUser) {
-            console.log(foundUser.validPassword(req.body.password))
-            if(foundUser.validPassword(req.body.password)) {
-                console.log("valid password")
-                req.session.message = '';
-                req.session.logged = true;
-                req.session.userDbId = foundUser._id;
-                console.log(req.session, ' successful in login')
-                res.redirect('/home');
-
-            } else {
-                req.session.message = "Username or password is incorrect";
-                res.redirect('/auth/login');
-            }
-        } else {
-            res.redirect('/auth/login');
-        }
-
-    } catch(err){
-        res.send(err);
-    }
-})
-
-router.get('/:id', (req, res) => {
-    User.findById(req.params.id)
-    .populate('tournament')
-    .exec((err, foundUser) => {
-        console.log(foundUser);
-        res.render('user/show.ejs', {
-            user: foundUser
-        })
-    })
-})
 
 
-router.delete('/:id', (req, res)=> {
-    User.findById(req.params.id, (err, deletedUser) => {
-      if(err){
-        res.send(err);
-      } else {
-        console.log(deletedUser);
-        Tour.deleteMany({
-          _id: {
-            $in: deletedUser.signedUp
-          }
-        }, (err, data) => {
-          console.log(data)
-          res.redirect(`/home`);
-        })
-      }
-    })
-  })
-  
+
 
 module.exports = router;
