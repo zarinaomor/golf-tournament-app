bbc8022069c3946dc78718d8f3c025e29983be7b
zarinaomor <zarinaomorova@yahoo.com>
2019-04-25 21:27:25 -0700

    changes

--- controllers/userC.js
+++ controllers/userC.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const User = require('../models/user');
 const Tour = require('../models/tournament');
 
-
 router.get('/new', (req,res) => {
     User.find({}, (err, allUsers) => {
             if(err) {
@@ -16,6 +15,32 @@ router.get('/new', (req,res) => {
         });
     })
 
+    router.post('/', async (req, res) => {
+        try {
+            const foundUser = await User.findOne({'email': req.body.email});
+            console.log(foundUser)
+            if(foundUser) {
+                console.log(foundUser.validPassword(req.body.password))
+                if(foundUser.validPassword(req.body.password)) {
+                    console.log("valid password")
+                    req.session.message = '';
+                    req.session.logged = true;
+                    req.session.userDbId = foundUser._id;
+                    console.log(req.session, ' successful in login')
+                    res.redirect('/home');
+    
+                } else {
+                    req.session.message = "Username or password is incorrect";
+                    res.redirect('/auth/login');
+                }
+            } else {
+                res.redirect('/auth/login');
+            }
+    
+        } catch(err){
+            res.send(err);
+        }
+    })
 
 
 module.exports = router;
