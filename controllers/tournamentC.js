b4d47ab53fbc9b5463a16d20c4036abc8e6c9d07
Kaitlin Park <kaitlinpark@parkaitlin-mac.local>
2019-04-29 11:47:20 -0700

    edit index page and tweek tournament model

--- controllers/tournamentC.js
+++ controllers/tournamentC.js
@@ -53,7 +53,7 @@ router.get('/host', (req, res)=>{
 
 
 router.get('/', async (req, res)=>{
-const foundTournaments = await Tournament.find({DateOfEvent: {$gt: req.session.userTimeStamp}}).sort({DateOfEvent: 1});
+const foundTournaments = await Tournament.find({eventDate: {$gt: req.session.userTimeStamp}}).sort({eventDate: 1});
 if (req.session.logged==true)try{
 res.render('tournaments/index.ejs', {
     tournaments: foundTournaments,
