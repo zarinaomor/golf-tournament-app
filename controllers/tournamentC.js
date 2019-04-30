850d5743318c20300ad9e6d4383281fed36ea803
Reed MacDonald <reedpmacdonald@gmail.com>
2019-04-30 10:11:07 -0700

    took out sort feature in index and made show page look nicer

--- controllers/tournamentC.js
+++ controllers/tournamentC.js
@@ -54,7 +54,7 @@ router.get('/host', (req, res)=>{
 
 
 router.get('/', async (req, res)=>{
-const foundTournaments = await Tournament.find({eventDate: {$gt: req.session.userTimeStamp}}).sort({eventDate: 1});
+const foundTournaments = await Tournament.find({})
 if (req.session.logged==true)try{
 res.render('tournaments/index.ejs', {
     tournaments: foundTournaments,
